be6bf835be5b30c80dac41c06e0cca1b68ddf175
Ridd418 <147160638+ridd418@users.noreply.github.com>
2025-11-23 15:38:20 +0530

    A2: Updated layout and styling, Updated data loading to load data directly from external JSON via fetch

--- script.js
+++ script.js
@@ -5,6 +5,44 @@ let userAnswers = {}
 const quizScreen = document.getElementById("quiz-screen")
 const resultScreen = document.getElementById("result-screen")
 
+// import {QUIZ_DATA} from './quiz-data.js'
+// import QUIZ_DATA from './quiz-data.js'
+
+const getData = async () => {
+    try {
+    const res = await fetch('./data.json')
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
+    const data = await res.json()
+    // console.log(data)
+    return data
+    } catch (err) {
+    console.error('Failed to load quiz data:', err)
+    }
+}
+
+// const getData = async () => {
+//     if (location.protocol.startsWith('http')) {
+//         try {
+//             const res = await fetch('./dat.json')
+//             if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
+//             const data = await res.json()
+//             // console.log(data)
+//             return data
+//         } catch (err) {
+//             console.warn('Failed to load quiz data:', err)
+//             // console.log('Loading from JS module!')
+//             // const module = await import('./quiz-data.js')
+//             // console.log(module.QUIZ_DATA)
+//             // return module.QUIZ_DATA
+//         }
+//     }
+//     data = await import('./quiz-data.js')
+//     console.log(data.QUIZ_DATA)
+//     return data.QUIZ_DATA
+// }
+
+const QUIZ_DATA = await getData()
+
 function renderQuestion() {
     const q = QUIZ_DATA.questions[currentQuestion]
 
